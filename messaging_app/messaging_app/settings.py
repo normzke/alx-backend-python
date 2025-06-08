995b1dc7127c0b0e5d10e18bb082e4fad5999fb5
normzke <your-email@example.com>
2025-06-08 23:05:26 +0300

    Add BasicAuthentication to settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = 'static/'
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'chats.auth.CustomJWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'chats.auth.CustomBasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
