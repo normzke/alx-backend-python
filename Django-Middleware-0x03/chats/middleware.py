9438d5c05d64a3f99884d6b14455b0fd6b07f80c
normzke <your-email@example.com>
2025-06-09 00:09:00 +0300

    Fix middleware class name to RolepermissionMiddleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -91,7 +91,7 @@ class OffensiveLanguageMiddleware:
             ip = request.META.get('REMOTE_ADDR')
         return ip
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # Define restricted actions that require admin/moderator role
